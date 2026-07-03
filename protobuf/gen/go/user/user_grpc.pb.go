926c9e453dac430bc5d85b3d46ca255d153bdb48
samurosa <75kno15co10s@gmail.com>
2026-07-03 17:11:47 +0300

    gen

--- protobuf/gen/go/user/user_grpc.pb.go
+++ protobuf/gen/go/user/user_grpc.pb.go
@@ -44,7 +44,7 @@ type UserServiceClient interface {
 	Deposit(ctx context.Context, in *DepositRequest, opts ...grpc.CallOption) (*DepositResponse, error)
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*TokenPairResponse, error)
 	Logout(ctx context.Context, in *RefreshTokenRequest, opts ...grpc.CallOption) (*LogoutResponse, error)
-	LogoutAllDevices(ctx context.Context, in *LogoutAllDevicesRequest, opts ...grpc.CallOption) (*LogoutResponse, error)
+	LogoutAllDevices(ctx context.Context, in *RefreshTokenRequest, opts ...grpc.CallOption) (*LogoutResponse, error)
 	GetBalances(ctx context.Context, in *UserIDRequest, opts ...grpc.CallOption) (*UserBalancesInfoResponse, error)
 	RefreshToken(ctx context.Context, in *RefreshTokenRequest, opts ...grpc.CallOption) (*TokenPairResponse, error)
 }
@@ -137,7 +137,7 @@ func (c *userServiceClient) Logout(ctx context.Context, in *RefreshTokenRequest,
 	return out, nil
 }
 
-func (c *userServiceClient) LogoutAllDevices(ctx context.Context, in *LogoutAllDevicesRequest, opts ...grpc.CallOption) (*LogoutResponse, error) {
+func (c *userServiceClient) LogoutAllDevices(ctx context.Context, in *RefreshTokenRequest, opts ...grpc.CallOption) (*LogoutResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(LogoutResponse)
 	err := c.cc.Invoke(ctx, UserService_LogoutAllDevices_FullMethodName, in, out, cOpts...)
@@ -179,7 +179,7 @@ type UserServiceServer interface {
 	Deposit(context.Context, *DepositRequest) (*DepositResponse, error)
 	Login(context.Context, *LoginRequest) (*TokenPairResponse, error)
 	Logout(context.Context, *RefreshTokenRequest) (*LogoutResponse, error)
-	LogoutAllDevices(context.Context, *LogoutAllDevicesRequest) (*LogoutResponse, error)
+	LogoutAllDevices(context.Context, *RefreshTokenRequest) (*LogoutResponse, error)
 	GetBalances(context.Context, *UserIDRequest) (*UserBalancesInfoResponse, error)
 	RefreshToken(context.Context, *RefreshTokenRequest) (*TokenPairResponse, error)
 	mustEmbedUnimplementedUserServiceServer()
@@ -216,7 +216,7 @@ func (UnimplementedUserServiceServer) Login(context.Context, *LoginRequest) (*To
 func (UnimplementedUserServiceServer) Logout(context.Context, *RefreshTokenRequest) (*LogoutResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method Logout not implemented")
 }
-func (UnimplementedUserServiceServer) LogoutAllDevices(context.Context, *LogoutAllDevicesRequest) (*LogoutResponse, error) {
+func (UnimplementedUserServiceServer) LogoutAllDevices(context.Context, *RefreshTokenRequest) (*LogoutResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method LogoutAllDevices not implemented")
 }
 func (UnimplementedUserServiceServer) GetBalances(context.Context, *UserIDRequest) (*UserBalancesInfoResponse, error) {
@@ -391,7 +391,7 @@ func _UserService_Logout_Handler(srv interface{}, ctx context.Context, dec func(
 }
 
 func _UserService_LogoutAllDevices_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(LogoutAllDevicesRequest)
+	in := new(RefreshTokenRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -403,7 +403,7 @@ func _UserService_LogoutAllDevices_Handler(srv interface{}, ctx context.Context,
 		FullMethod: UserService_LogoutAllDevices_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UserServiceServer).LogoutAllDevices(ctx, req.(*LogoutAllDevicesRequest))
+		return srv.(UserServiceServer).LogoutAllDevices(ctx, req.(*RefreshTokenRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }

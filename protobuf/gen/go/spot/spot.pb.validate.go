09d27d3237a7eb7accfaad3af8a44d8eeced888c
samurosa <75kno15co10s@gmail.com>
2026-07-29 14:45:33 +0300

    remake type allowed_roles

--- protobuf/gen/go/spot/spot.pb.validate.go
+++ protobuf/gen/go/spot/spot.pb.validate.go
@@ -17,6 +17,8 @@ import (
 	"unicode/utf8"
 
 	"google.golang.org/protobuf/types/known/anypb"
+
+	user "github.com/Samurosa/exchange-contract/protobuf/gen/go/user"
 )
 
 // ensure the imports are used
@@ -33,6 +35,8 @@ var (
 	_ = (*mail.Address)(nil)
 	_ = anypb.Any{}
 	_ = sort.Sort
+
+	_ = user.Role(0)
 )
 
 // define the regex for a UUID once up-front
